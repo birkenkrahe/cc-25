c7b5e515657c070d1b59752f9f2e11354c73ff5d
birkenkrahe <birkenkrahe@lyon.edu>
2025-02-26 10:24:59 -0600

    Update from dell

--- src/boolean.c
+++ src/boolean.c
@@ -9,11 +9,16 @@
 int main() {
     int x = 1, y = 0, z = 5;
 
-    printf("%d ? %d\n", x && y, XX);  // XX: replace by what you expect
-    printf("%d ? %d\n", x || y, XX);
-    printf("%d ? %d\n", x == y, XX);
-    printf("%d ? %d\n", !x, XX);    
-    printf("%d ? %d\n", z > x && y < z, XX); 
+    // Below, replace `?` by what you expect the Boolean expression
+    // result in - for example for the first statement, compute (in
+    // your head) `1 && 0` (which is 1 AND 0 in the truth table, and
+    // replace `?` by what you think the result will be.
+    
+    printf("%d == %d\n", x && y, ?);  
+    printf("%d == %d\n", x || y, ?);
+    printf("%d == %d\n", x == y, ?);
+    printf("%d == %d\n", !x, ?);    
+    printf("%d == %d\n", z > x && y < z, ?); 
 
     return 0;
 }
