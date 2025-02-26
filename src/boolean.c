851001c235550011b3e40e129b384f95a821c4ff
birkenkrahe <birkenkrahe@lyon.edu>
2025-02-26 11:58:35 -0600

    Update from dell

--- src/boolean.c
+++ src/boolean.c
@@ -1,24 +1,25 @@
-/****************************************
-* boolean.c: Predict Boolean results    *
-* Input: None. Output: Boolean values   *
-* Author: Marcus Birkenkrahe GPLv3      *
-* Date: 02/22/2025                      *
-****************************************/
+#include <stdio.h>
+
+
+
+
+
+
 #include <stdio.h>
 
 int main() {
     int x = 1, y = 0, z = 5;
 
-    // Below, replace `?` by what you expect the Boolean expression
-    // result in - for example for the first statement, compute (in
-    // your head) `1 && 0` (which is 1 AND 0 in the truth table, and
-    // replace `?` by what you think the result will be.
-    
-    printf("%d == %d\n", x && y, ?);  
-    printf("%d == %d\n", x || y, ?);
-    printf("%d == %d\n", x == y, ?);
-    printf("%d == %d\n", !x, ?);    
-    printf("%d == %d\n", z > x && y < z, ?); 
+  // Below, replace `?` by what you expect the Boolean expression
+  // result in - for example for the first statement, compute (in
+  // your head) `1 && 0` (which is 1 AND 0 in the truth table, and
+  // replace `?` by what you think the result will be.
+
+  printf("%d == %d\n", x && y, ?);
+  printf("%d == %d\n", x || y, ?);
+  printf("%d == %d\n", x == y, ?);
+  printf("%d == %d\n", !x, ?);
+  printf("%d == %d\n", z > x && y < z, ?);
 
     return 0;
 }
