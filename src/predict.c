bfe0e11720817cfab1a25fc1534915c94671a366
birkenkrahe <birkenkrahe@lyon.edu>
2025-02-27 21:15:28 -0600

    Update from thelio

--- src/predict.c
+++ src/predict.c
@@ -4,23 +4,23 @@
 * Author: Marcus Birkenkrahe GPLv3      *
 * Date: 02/22/2025                      *
 ****************************************/
-#include <stdio.h>
-
-int main() {
-  int a = 5, b = 10, c;
-
-  c = a + b * 2;
-  printf("1. c = %d ? %d\n", c, XX); // replace XX by your guess
-
-  c = (a + b) * 2;
-  printf("2. c = %d ? %d\n", c, XX);
-
-  c = b / a + 3;
-  printf("3. c = %d ? %d\n", c, XX);
-
-  c = ++a + b--;
-  printf("4. a = %d ? %d, b = %d ? %d, c = %d ? %d\n", 
-         a, XX, b, XX, c, XX);
-
-  return 0;
-}
+#include <stdio.h>                                                   
+                                                                     
+int main() {                                                         
+  int a = 5, b = 10, c;                                              
+                                                                     
+  c = a + b * 2;                                                     
+  printf("1. c = %d == %d\n", c, ?); // what is c
+                                                                     
+  c = (a + b) * 2;                                                   
+  printf("2. c = %d == %d\n", c, ?);                                 
+                                                                     
+  c = b / a + 3;                                                     
+  printf("3. c = %d == %d\n", c, ?);                                 
+                                                                     
+  c = ++a + b--;                                                     
+  printf("4. a = %d == %d, b = %d == %d, c = %d == %d\n",            
+         a, ?, b, ?, c, ?);                                          
+                                                                     
+  return 0;                                                          
+}                                                                    
